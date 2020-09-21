a4cbf56717a7d7fa329197175c91c6594e28eb43
Spasov <i.spasov@sap.com>
2020-09-21 17:15:33 +0300

    Commit Pipeline v6

--- update_date.sh
+++ update_date.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
 cd concourse-tasks
-# adsd
+
 git config --global user.email "johnspasov99@gmail.com"
 git config --global user.name "ivanspasov99"
 
-date > date
+date > dateg
 
-git status
 git add .
-
 git status
-
 git commit -m "New Changes"
-
-git status
-
 git push origin master
